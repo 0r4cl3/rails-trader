2dcfdeaa6cd4a25f538cbe56a39c36257e36b0b7
Luca Licheri <lucalicheri@gmail.com>
2015-10-14 00:04:56 +0200

    refactoring controller

--- app/controllers/oanda_api_controller.rb
+++ app/controllers/oanda_api_controller.rb
@@ -4,9 +4,37 @@ class OandaApiController < ApplicationController
 
   def index
     @candles = GetCandles.new(@client, granuality: 'M15').fetch_candles
+    @london_time = get_london_time
+    @newyork_time = get_newyork_time
+    @tokio_time = get_tokio_time
+    @sydney_time = get_sydney_time
+
+    @time = Time.now.utc.hour
   end
 
   def show
 
+  end
+
+  private
+
+  def dst
+    @dst ||= Time.now.dst?
+  end
+
+  def get_london_time
+    dst == true ? (Time.now.utc + 3600).hour : Time.now.utc.hour
+  end
+
+  def get_newyork_time
+    dst == true ? (Time.now.utc - 3600*4).hour : (Time.now.utc - 3600*5).hour
+  end
+
+  def get_tokio_time
+    (Time.now.utc + (3600 * 9)).hour
+  end
+
+  def get_sydney_time
+    dst == true ? (Time.now.utc + 3600*11).hour : (Time.now.utc + 3600*10).hour
   end
 end
