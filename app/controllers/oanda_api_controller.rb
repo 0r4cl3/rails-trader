ab132406d3731587e97d8351d4bb412d09bd6d76
Luca Licheri <lucalicheri@gmail.com>
2015-11-11 13:06:15 +0100

    Moved renders to appropriate pages

--- app/controllers/oanda_api_controller.rb
+++ app/controllers/oanda_api_controller.rb
@@ -3,6 +3,21 @@ class OandaApiController < ApplicationController
 
 
   def index
+    @rsi = Rsi.new(@candles).calculate_rsi
+  end
+
+  def calendar
+    @london_time = GetTime.new.london
+    @newyork_time = GetTime.new.newyork
+    @sydney_time = GetTime.new.sydney
+    @tokio_time = GetTime.new.tokio
+    @london_market = Market.new(@london_time).open?
+    @sydney_market = Market.new(@sydney_time).open?
+    @newyork_market = Market.new(@newyork_time).open?
+    @tokio_market = Market.new(@tokio_time).open?
+  end
+
+  def candles 
     @instrument = params[:instrument] || 'EUR_USD'
     @options_for_instrument = [['EUR_USD', 'EUR_USD'], ['AUD_CAD', 'AUD_CAD']]
     @granularity = params[:granularity] || 'M15'
@@ -10,18 +25,6 @@ class OandaApiController < ApplicationController
     @count = params[:count] || 100
     @options_for_count = [[10, 10], [20, 20]]
     @candles = GetCandles.new(@client, instrument: @instrument, granularity: @granularity, count: @count).fetch_candles
-
-    @london_time = GetTime.new.london
-    @newyork_time = GetTime.new.newyork
-    @sydney_time = GetTime.new.sydney
-    @tokio_time = GetTime.new.tokio
-
-    @london_market = Market.new(@london_time).open?
-    @sydney_market = Market.new(@sydney_time).open?
-    @newyork_market = Market.new(@newyork_time).open?
-    @tokio_market = Market.new(@tokio_time).open?
-
-    @rsi = Rsi.new(@candles).calculate_rsi
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :oanda_api
   root 'oanda_api#index'
+  get 'calendar' => 'oanda_api#calendar'
+  get 'candles' => 'oanda_api#candles'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
