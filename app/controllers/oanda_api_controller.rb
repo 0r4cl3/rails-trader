e1b0976288c227e4bc75f8c532976b4577dda331
Luca Licheri <lucalicheri@gmail.com>
2015-11-11 07:34:20 +0100

    Added instrument selection tag

--- app/controllers/oanda_api_controller.rb
+++ app/controllers/oanda_api_controller.rb
@@ -3,12 +3,14 @@ class OandaApiController < ApplicationController
 
 
   def index
+    @instrument = params[:instrument] || 'EUR_USD'
+    @options_for_instrument = [['EUR_USD', 'EUR_USD'], ['AUD_CAD', 'AUD_CAD']]
     @granularity = params[:granularity] || 'M15'
     @options_for_granularity = [['M1', 'M1'], ['M15', 'M15'], ['M30', 'M30']]
     @count = params[:count] || 100
     @options_for_count = [[10, 10], [20, 20]]
-    @candles = GetCandles.new(@client, granularity: @granularity, count: @count).fetch_candles
-    
+    @candles = GetCandles.new(@client, instrument: @instrument, granularity: @granularity, count: @count).fetch_candles
+
     @london_time = GetTime.new.london
     @newyork_time = GetTime.new.newyork
     @sydney_time = GetTime.new.sydney
--- app/models/get_candles.rb
+++ app/models/get_candles.rb
@@ -1,14 +1,15 @@
 class GetCandles
   attr_reader :candles, :client
 
-  def initialize(client, params)
+  def initialize(client, params) 
+    @instrument = params[:instrument]
     @granularity = params[:granularity]
     @count = params[:count]
     @client = client
   end
 
   def fetch_candles
-    @candles = client.candles( instrument: "EUR_USD",
+    @candles = client.candles( instrument: @instrument,
                               granularity: @granularity,
                             candle_format: "midpoint",
                                     count: @count).get
