e9b5abc3a52044850d948a4a605c0823710241df
Luca Licheri <lucalicheri@gmail.com>
2015-11-22 21:43:31 +0100

    Create orders controller and moved data into it

--- app/controllers/oanda_api_controller.rb
+++ app/controllers/oanda_api_controller.rb
@@ -29,15 +29,6 @@ class OandaApiController < ApplicationController
     @account_currency = @account.account_currency
   end
 
-  def orders
-    @instrument = params[:instrument] || "EUR_USD" 
-    @type = params[:type] || "market"
-    @options_for_type = [['market', 'market']]
-    @side = params[:side] || "buy" 
-    @options_for_side = [['buy', 'buy'], ['sell', 'sell']]
-    @units = params[:units] || 100
-    @order = Order.new(@client, @account_number, instrument: @instrument, type: @type, side: @side, units: @units).place
-  end
 
 private
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,16 +4,20 @@ class OrdersController < ApplicationController
   before_action { @account_number = Rails.application.secrets.oanda_account }
   
   def index
+  end
+
+  def create
+    @order = Order.new(@client, @account_number, instrument: @instrument, type: @type, side: @side, units: @units)
+    @order.place
+  end
+
+  def new
     @instrument = params[:instrument] || "EUR_USD" 
     @type = params[:type] || "market"
     @options_for_type = [['market', 'market']]
     @side = params[:side] || "buy" 
     @options_for_side = [['buy', 'buy'], ['sell', 'sell']]
     @units = params[:units] || 100
-
-    @order = Order.new(@client, @account_number, instrument: @instrument, type: @type, side: @side, units: @units)
   end
 
-  def create
-  end
 end
